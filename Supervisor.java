e9239b9e46e1418e89ca4c9d80e43222346d6b96
anne27 <anannyauberoi27@gmail.com>
2016-11-12 19:48:32 +0530

    Holiday

--- Supervisor.java
+++ Supervisor.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.util.*;
 public class Supervisor {
 	private String ID,username,name,department,DOB,address;
@@ -84,7 +86,21 @@ public class Supervisor {
 	}
 	void deleteStaff(Staff staff)
 	{
+		this.no_of_staff--;
+		this.staff_under_me.remove(staff);
+		//rewrite file
+		File myFile = new File("Supervisor.csv");
 		
+		/*try
+		{
+			FileWriter fileWriter = new FileWriter(myFile,true);
+            StringBuilder string1=new StringBuilder();
+           // string1.append("\r\n"+text+","+selecteditem+","+staff.getID()+","+staff.getDepartment()+","+"Unapproved");
+            fileWriter.write(string1.toString());
+            System.out.println(string1);
+            fileWriter.close();
+		}
+		catch(Exception e){}*/
 	}
 	void assignTask(Task task)	//pass parameter to assign to only 1, some or all
 	{
